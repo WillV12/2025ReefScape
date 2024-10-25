735a74e6a401e62058f0279219b300d3b6030080
Will Vanderploeg <thatmurphyboi@gmail.com>
2024-10-24 20:28:08 -0400

    Added swerve and autoselector

---
+++ src/main/java/frc/robot/autos/AutoSelector.java
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+/* 
+package frc.robot.autos;
+
+
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.swervedrive.SwerveSubsystem;
+*/
+
+/** Add your docs here. */
+/* 
+public class AutoSelector {
+      private final SendableChooser<Command> chooser = new SendableChooser<>();
+
+
+      public AutoSelector();
+
+            chooser.addOption("Two Shot");
+      }
+      public Command getSelected(){
+            return chooser.getSelected();
+      } 
+}
+*/
